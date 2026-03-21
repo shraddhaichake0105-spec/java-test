f392dea6045ff9d253a68798c829f0a460f512a6
shraddha ichake <shraddhaichake0105@gmail.com>
2026-03-21 21:44:59 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int a=9;
+        int b=3;
+        int c= a+b;
+        
+        
+        System.out.println("Try programiz.pro"+c);
+    }
+}
