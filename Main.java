20f49b39f56f9599d21cea85830469f8331c9744
shraddha ichake <shraddhaichake0105@gmail.com>
2026-03-21 21:48:27 +0530

    second commit

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ class Main {
         int a=9;
         int b=3;
         int c= a+b;
-        
-        
-        System.out.println("Try programiz.pro"+c);
+        int d = a*b;
+
+        System.out.println("multiply"+d);
     }
 }
